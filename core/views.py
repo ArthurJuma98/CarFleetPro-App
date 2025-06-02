5fc20307ab4dafce26e542761e9bec5ba38e5c5a
Arthur Ochilo <arthurjuma98@gmail.com>
2025-06-03 00:42:50 +0300

    changes in views

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
-    categories = Category.objects.all()
+    categories = Category.objects.first()
 
     return render(request, 'core/index.html', {
         'categories': categories,
